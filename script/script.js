e5a9326a74cf686aa1fa0a95a3d5d335348dbe39
Wonders Victor <wondersprince1@gmail.com>
2025-02-03 22:11:11 +0100

    Working

--- script/script.js
+++ script/script.js
@@ -1,9 +1,21 @@
 const showPsswd  = document.getElementById('toggle-password');
+const pswdInput = document.getElementById('password');
+
+pswdInput.addEventListener('focus', () => {
+    // console.log('focus');
+    showPsswd.style.display = 'block';
+})
+
+document.addEventListener('click', (e) => {
+    if (e.target !== showPsswd && e.target !== pswdInput) {
+        showPsswd.style.display = 'none';
+    }
+})
+
 
 showPsswd.addEventListener('click', () => {
-    console.log('toggle password');
-    const pswdInput = document.getElementById('password');
-    console.log(pswdInput);
+    // console.log('toggle password');
+    // console.log(pswdInput);
     pswdInput.type = pswdInput.type === 'password'? 'text' : 'password';  // Toggle between 'password' and 'text' types
     showPsswd.textContent = pswdInput.type === 'password'? 'Show' : 'Hide'; // Update the button text
 })
